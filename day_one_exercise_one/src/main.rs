bbc820103d970007d02f5bc29d96ca8a2561fb9c
James Daly <james@codenomads.nl>
2020-12-01 10:11:50 +0100

    Adding rust files

---
+++ day_one_exercise_one/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main() {
+    let filename = "data.txt";
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    let numbers: Vec<i32> = contents.split("\n").map(|value| value.parse().unwrap()).collect();
+    for n1 in numbers.clone() {
+        for n2 in numbers.clone() {
+            if (n1 + n2) == 2020 {
+                println!("{} + {} = {}", n1, n2, n1+n2);
+                println!("Answer is equal to {}", n1*n2);
+                return;
+            }
+        }
+    }
+}
